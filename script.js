ca20cdebb56c262f5f9849212cf3744c8ab94684
nicolas carvalho ferreira <139086434+NICOLASferreira018@users.noreply.github.com>
2023-11-01 21:21:55 -0300

    first commit

--- script.js
+++
@@ -1,9 +0,0 @@
-const countElement = document.getElementById('count');
-const incrementButton = document.getElementById('incrementButton');
-
-let count = 0;
-
-incrementButton.addEventListener('click', () => {
-  count++;
-  countElement.textContent = count;
-});
